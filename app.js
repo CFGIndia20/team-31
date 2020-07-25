e398328356bb90dd826bacdc2422e0534fc4c0aa
Aadil Sachdev <aadilsachdev24@gmail.com>
2020-07-25 07:14:28 +0000

    Initialize express, request, bodyparser and mongoose

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Welcome to The Nudge Web App");
+var express=require("express");
+var app=express();
+var request=require("request");
+var bodyParser=require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+var mongoose=require("mongoose");
+mongoose.connect("mongodb://localhost/yelp_camp",{useNewUrlParser: true, useUnifiedTopology: true});
+
